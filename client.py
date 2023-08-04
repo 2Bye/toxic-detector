a6af1232770c57914246ba015256f8da1c4d7d48
root <ajhtcn9@gmail.com>
2023-08-04 17:13:08 +0300

    Change Logic, add prints

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ from websocket import create_connection
 uri = "ws://0.0.0.0:6612/"
 ws = create_connection(uri, ping_interval=None)
 
-user_message = 'Niggers is shit'
+user_message = 'Hello!'
 data = {'user_message' : user_message}
 ws.send(json.dumps(data)) ### send data
 responce = json.loads(ws.recv())
--- detox.py
+++ detox.py
@@ -8,7 +8,7 @@ class FilterModel():
         self.adult_content_label_map = ["Non-sex", "Sex"]
 
         self.toxicity_model_name = "unitary/multilingual-toxic-xlm-roberta"
-        self.adult_content_model_name = "ziadA123/adultcontentclassifier"
+        self.adult_content_model_name = "ziadA123/autotrain-adult-classification-3642997339"
 
         self.toxicity_tokenizer = AutoTokenizer.from_pretrained(self.toxicity_model_name)
         self.toxicity_model = AutoModelForSequenceClassification.from_pretrained(self.toxicity_model_name)
@@ -29,19 +29,28 @@ class FilterModel():
         with torch.no_grad():
             outputs = self.adult_content_model(**inputs)
             logits = outputs.logits
-            probabilities = torch.sigmoid(logits)
-        return probabilities.tolist()[0]
+            probabilities_sigmoid, probabilities_argmax = torch.sigmoid(logits), torch.argmax(logits)
+        return probabilities_sigmoid.tolist(), probabilities_argmax
     
-    def get_label(self, text: str) -> str:
+    def get_label(self, text: str) -> list:
         toxic_probability = self.classify_toxicity(text)
-        toxic_prediction = 1 if toxic_probability > 0.5 else 0
+        toxic_prediction = 1 if toxic_probability > 0.65 else 0
         toxic_class_label = self.toxic_label_map[toxic_prediction]
 
-        adult_content_probability = self.classify_adult_content(text)
-        adult_content_prediction = 1 if adult_content_probability[0] < 0.5 else 0
-        adult_content_class_label = self.adult_content_label_map[adult_content_prediction]
-
+        adult_content_probability_sigmoid, adult_content_probability_argmax = self.classify_adult_content(text)
+        adult_content_label = 1 if adult_content_probability_sigmoid[0][1] > 0.75 else 0
+        adult_content_class_label = self.adult_content_label_map[adult_content_label]
+        
+        print('')
+        print((f'// Input Text\t\t\t\t| {text}'))
+        print('_' * 10)
+        print('// Toxic outputs\t\t\t|', 'Hugging Face model')
+        print('// Toxic probability\t\t\t|', toxic_probability)
+        print('// Result\t\t\t\t|', toxic_class_label)
+        print('_' * 10)
+        print('// Adult outputs\t\t\t|', 'Hugging Face model')
+        print('// ArgMax Function\t\t\t|', adult_content_probability_argmax)
+        print('// Sigmoid Function\t\t\t|', adult_content_probability_sigmoid)
+        print('// Result\t\t\t\t|', adult_content_class_label)
+        print('')
         return [toxic_class_label, adult_content_class_label]
-    
-
-
